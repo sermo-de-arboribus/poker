c4021baa9b661b6ef76bba8004728ddedfb928f9
Kai Weber <sermo_de_arboribus@seznam.cz>
2022-10-22 10:45:02 +0200

    Added JavaDoc comments to CardHand class

--- src/main/java/poker/model/CardHand.java
+++ src/main/java/poker/model/CardHand.java
@@ -11,11 +11,26 @@ import java.util.TreeSet;
 
 import poker.error.HandSizeError;
 
+/**
+ * This class represents a full hand of five Poker cards. When the hand is initialized or changed, the hand's rank 
+ * gets evaluated. CardHands can be compared (ranked) against each other to figure out which hand wins.
+ *
+ */
 public class CardHand {
 
 	private SortedSet<Card> hand = new TreeSet<Card>();
 	private Rank rank = Rank.HIGH_CARD;
 	
+	/************************ Constructors ****************************/
+	
+	/**
+	 * Construct a hand of Poker cards out of five individual Card objects
+	 * @param card1 card object #1
+	 * @param card2 card object #2
+	 * @param card3 card object #3
+	 * @param card4 card object #4
+	 * @param card5 card object #5
+	 */
 	public CardHand(final Card card1, final Card card2, final Card card3, final Card card4, final Card card5) {
 		Card[] cards = new Card[5];
 		cards[0] = card1;
@@ -30,18 +45,40 @@ public class CardHand {
 		}
 	}
 	
+	/**
+	 * Construct a hand or Poker card out of a Card array. The array is expected to provide exactly five cards.
+	 * 
+	 * @param cards A Card array of length 5
+	 * @throws HandSizeError is thrown when array has less or more than 5 Card objects
+	 */
 	public CardHand(final Card[] cards) throws HandSizeError {
 		resetHand(cards);
 	}
 	
+	/**
+	 * Construct a hand or Poker card out of a Card list. The list is expected to provide exactly five cards.
+	 * 
+	 * @param cards A Card list of length 5
+	 * @throws HandSizeError is thrown when the list has less or more than 5 Card objects
+	 */
 	public CardHand(final List<Card> cards) throws HandSizeError {
 		Card[] cardsArray = cards.toArray(new Card[0]);
 		resetHand(cardsArray);
 	}
 	
+
+	
+
+	/**
+	 * Get the current rank of the five cards on this hand.
+	 * 
+	 * @return The Rank that this hand currently evaluates to
+	 */
 	public Rank getRank() {
 		return rank;
 	}
+	
+	/************************ Private methods *************************/
 	
 	/**
 	 * Contract of this class: This method should be called internally whenever the card set in the `hand` variable changes 
